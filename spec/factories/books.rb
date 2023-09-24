1c2c74dac4365e6457db471f11362c9856a67441
daikimatsuoka <daiki19981115@gmail.com>
2023-09-24 09:39:23 +0900

    単体テストの実装

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :book do
-    
+    title                {Faker::Book.title}
+    author               {Faker::Book.author}
+    publisher            {Faker::Book.publisher}
+    category_id          {Faker::Number.between(from: 2, to: 33)}
+    synopsis_explanation {Faker::Lorem.paragraph(sentence_count: 3)}
+    association :user
+
+    after(:build) do |book|
+      book.book_image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @book = FactoryBot.build(:book)
+  end
+
+  describe '書籍投稿機能' do
+    context '投稿できるとき' do
+      it 'すべて正しく入力されていれば登録できる' do
+        expect(@book).to be_valid
+      end
+    end
+
+    context '投稿できないとき' do
+      it 'titleが空では登録できない' do
+        @book.title = ''
+        @book.valid?
+        expect(@book.errors.full_messages).to include "Title can't be blank"
+      end
+
+      it 'authorが空では登録できない' do
+        @book.author = ''
+        @book.valid?
+        expect(@book.errors.full_messages).to include "Author can't be blank"
+      end
+
+      it 'publisherが空では登録できない' do
+        @book.publisher = ''
+        @book.valid?
+        expect(@book.errors.full_messages).to include "Publisher can't be blank"
+      end
+
+      it 'category_idが1では登録できない' do
+        @book.category_id = 1
+        @book.valid?
+        expect(@book.errors.full_messages).to include "Category can't be blank"
+      end
+      
+      it 'synopsis_explanationが空では登録できない' do
+        @book.synopsis_explanation = ''
+        @book.valid?
+        expect(@book.errors.full_messages).to include "Synopsis explanation can't be blank"
+      end
+
+      it 'book_imageが空では登録できない' do
+        @book.book_image = nil
+        @book.valid?
+        expect(@book.errors.full_messages).to include "Book image can't be blank"
+      end
+
+      it 'userが紐付いていなければ登録できない' do
+        @book.user = nil
+        @book.valid?
+        expect(@book.errors.full_messages).to include "User must exist"
+
+      end
+    end
+  end
+
 end
