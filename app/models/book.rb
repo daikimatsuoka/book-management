47b74d43d454df15d32698c5bb8c5cf8d9e64686
daikimatsuoka <daiki19981115@gmail.com>
2023-09-25 18:38:39 +0900

    コメント機能の実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,5 +10,6 @@ class Book < ApplicationRecord
   belongs_to :category
 
   belongs_to :user
+  has_many :comments
   has_one_attached :book_image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
+  has_many :comments
   
   validates :nickname, presence: true, length: { maximum: 6 }
 end
