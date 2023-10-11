cad94066bf5155cb6ef0bb4578465e2b90e66a32
daikimatsuoka <daiki19981115@gmail.com>
2023-10-11 14:23:01 +0900

    いいねボタンの非同期化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,19 @@
 class LikesController < ApplicationController
+before_action :set_tweet
+
   def create
     like = current_user.likes.build(book_id: params[:book_id])
     like.save
-    redirect_to root_path
+    render 'create.js.erb'
   end
 
   def destroy
     like = Like.find_by(book_id: params[:book_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    render 'destroy.js.erb'
+  end
+
+  def set_tweet
+    @book = Book.find(params[:book_id])
   end
 end
