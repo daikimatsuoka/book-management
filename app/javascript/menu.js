09a5a4c3c047097a821773c876a6d6ea4845fcd1
daikimatsuoka <daiki19981115@gmail.com>
2023-10-21 08:33:15 +0900

    javascriptの導入、HTMLの取得

---
+++ app/javascript/menu.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+  const menu1 = document.getElementsByClassName("right-btn")
+  const menu2 = document.getElementsByClassName("left-btn")
+  console.log(menu1)
+  console.log(menu2)
+});
