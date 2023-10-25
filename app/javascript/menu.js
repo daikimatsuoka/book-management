39c9e7ebe834019ed81dac384947e8ac6349ed31
daikimatsuoka <daiki19981115@gmail.com>
2023-10-25 12:59:47 +0900

    ヘッダーボタンに対する非同期通信の実装

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,6 +1,20 @@
 window.addEventListener('load', function(){
-  const menu1 = document.getElementsByClassName("right-btn")
-  const menu2 = document.getElementsByClassName("left-btn")
-  console.log(menu1)
-  console.log(menu2)
+  const menu1 = document.getElementsByClassName("right-btn")[0];
+  const menu2 = document.getElementsByClassName("left-btn")[0];
+
+  menu1.addEventListener('mouseover', function(){
+    menu1.setAttribute("style", "background-color:#69b076;")
+  });
+
+  menu1.addEventListener('mouseout', function(){
+    menu1.removeAttribute("style")
+  });
+
+  menu2.addEventListener('mouseover', function(){
+    menu2.setAttribute("style", "background-color:#69b076;")
+  });
+
+  menu2.addEventListener('mouseout', function(){
+    menu2.removeAttribute("style")
+  });
 });
