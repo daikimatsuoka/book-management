a47c2586f77a5b594fb0248109c23319d62e9f56
daikimatsuoka <daiki19981115@gmail.com>
2023-10-30 12:07:33 +0900

    コンテンツに非同期通信動作を付与

--- app/javascript/content.js
+++ app/javascript/content.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', function(){
-  console.log('')
+  const books = document.querySelectorAll(".book");
+  books.forEach(function(book) {
+    book.addEventListener('mouseover', function() {
+      this.setAttribute("style", "box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);");
+    });
+    book.addEventListener('mouseout', function() {
+      this.removeAttribute("style", "box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);");
+    });
+  });
 })
